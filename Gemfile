da4c5304a94bf56a77baac5226d4c05b4b23fc1d
Wojciech Winnicki <wbwinnicki@gmail.com>
2017-06-19 21:20:00 +0200

    Add user micropost

--- Gemfile
+++ Gemfile
@@ -12,8 +12,12 @@ gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.11'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'faker', '1.7.3'
+gem 'carrierwave',             '1.1.0'
+gem 'mini_magick',             '4.7.0'
+gem 'fog',                     '1.40.0'
 gem 'will_paginate', '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
+
 group :development, :test do
   gem 'sqlite3', '1.3.13'
   gem 'byebug',  '9.0.6', platform: :mri
---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User with 0 posts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
+  end
+end
